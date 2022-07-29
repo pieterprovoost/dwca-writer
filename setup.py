bcff932fddc4603b6292c148d84916c0c0eeb77a
Pieter Provoost <pieterprovoost@gmail.com>
2022-07-29 21:44:15 +0200

    fixed long description

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 from setuptools import setup, find_packages
-import os
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert_file("README.md", "rst")
+except(IOError, ImportError):
+    long_description = open("README.md").read()
 
 
 setup(name="dwca-writer",
-      version="0.0.7",
+      version="0.0.8",
       author="Pieter Provoost",
       author_email="pieterprovoost@gmail.com",
       description="Python package for writing Darwin Core Archives (DwC-A)",
-      long_description=read("README.md"),
+      long_description=long_description,
       url="https://github.com/pieterprovoost/dwca-writer",
       license="MIT",
       packages=find_packages(),
