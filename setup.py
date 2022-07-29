18c7353a1b33f8364afcabfba617f838e2c30402
Pieter Provoost <pieterprovoost@gmail.com>
2022-07-29 21:37:23 +0200

    added long description

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup, find_packages
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 setup(name="dwca-writer",
-      version="0.0.6",
+      version="0.0.7",
       author="Pieter Provoost",
       author_email="pieterprovoost@gmail.com",
       description="Python package for writing Darwin Core Archives (DwC-A)",
+      long_description=read("README.md"),
+      url="https://github.com/pieterprovoost/dwca-writer",
       license="MIT",
       packages=find_packages(),
       zip_safe=False,
